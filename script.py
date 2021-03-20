d15d4c112891a0056ebd18632604ef92c4d7227c
Misha Farber Brodsky <misha@farberbrodsky.com>
2021-03-20 12:27:19 +0200

    You can run it with a script

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
 import os
+import sys
 import json
+import shlex
 import getpass
 import argparse
 import subprocess
 from appdirs import AppDirs
 from pathlib import Path
+from _thread import interrupt_main
+from threading import Timer
 
 TUNNELING_PORT="48097"
 
@@ -27,6 +31,7 @@ my_parser = argparse.ArgumentParser(
         description="My CLI to upload folders and get shell in nova.cs.tau.ac.il"
 )
 my_parser.add_argument("--upload", type=Path, help="Uploads this before connecting", nargs="*")
+my_parser.add_argument("--script", type=Path, help="Run this Python script instead of connecting")
 args = my_parser.parse_args()
 
 # Connect to gate
@@ -36,12 +41,21 @@ tunnel_proc = subprocess.Popen(
          "-ssh", "-L", TUNNELING_PORT + ":nova.cs.tau.ac.il:22",
          auth_data["username"] + "@gate.tau.ac.il"],
         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-tunnel_line = tunnel_proc.stdout.readline()
-print("tunneling log:", tunnel_line)
-while not tunnel_line.startswith(b"Welcome"):
+
+# if it takes more than 5 seconds to reach Last login, it's a failed attempt
+timeout_timer = Timer(5, interrupt_main)
+
+try:
+    timeout_timer.start()
     tunnel_line = tunnel_proc.stdout.readline()
-    if len(tunnel_line) > 0:
-        print("tunneling log:", tunnel_line)
+    while not tunnel_line.startswith(b"Last login"):
+        tunnel_line = tunnel_proc.stdout.readline()
+        if len(tunnel_line) > 0:
+            print("tunneling log:", tunnel_line)
+    timeout_timer.cancel()
+except:
+    print("Couldn't connect to tunnel - either an incorrect password or no internet.")
+
 print("Tunneling connected on port", TUNNELING_PORT)
 
 for upload in args.upload or []:
@@ -53,5 +67,22 @@ for upload in args.upload or []:
              "-scp", "-r", "-P", TUNNELING_PORT,
              upload, auth_data["username"] + "@localhost:" + upload.parts[-1]], check=True))
 # connect to Nova
-os.system(f"plink -pw {auth_data['password']} -hostkey b5:ee:e3:25:dd:35:94:c4:c1:98:f0:16:e9:3a:42:f8 -ssh -P {TUNNELING_PORT} {auth_data['username']}@localhost")
+conn_command = f"plink -pw {auth_data['password']} -hostkey b5:ee:e3:25:dd:35:94:c4:c1:98:f0:16:e9:3a:42:f8 -ssh -P {TUNNELING_PORT} {auth_data['username']}@localhost"
+if args.script:
+    # copy script
+    print(subprocess.run(
+            ["pscp", "-pw", auth_data['password'],
+             "-hostkey", "b5:ee:e3:25:dd:35:94:c4:c1:98:f0:16:e9:3a:42:f8",
+             "-scp", "-r", "-P", TUNNELING_PORT,
+             args.script.resolve(), auth_data["username"] + "@localhost:script.py"], check=True))
+    # run script
+    script_proc = subprocess.Popen(shlex.split(conn_command) + ["python3 script.py && rm script.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    script_proc.wait()
+    print(script_proc.stdout.read().decode("utf-8"))
+    if script_proc.returncode == 0:
+        print("\033[32mSuccess!\033[37m", end="")
+    else:
+        print("\033[31mERROR!\033[37m", end="")
+else:
+    os.system(conn_command)
 tunnel_proc.kill()
